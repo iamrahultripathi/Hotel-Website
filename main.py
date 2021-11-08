ebd9eea3eb7e5517dd15f97fb4e63909be4e98b3
NityaGada <87537845+NityaGada@users.noreply.github.com>
2021-11-08 19:58:11 +0530

    Co-authored-by: pratyushd02 <pratyushd02@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import re
+from flask import Flask,render_template,redirect,request
+from flask.helpers import get_load_dotenv, total_seconds
+from model1 import db,Feedback
+
+app = Flask(name,template_folder='templates')
+app.secret_key = 'super secret key'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        location=request.form['dropdown']
+        ordertype=request.form['papa']
+        improvement=improvement
+        message=request.form['message']
+        
+        
+        feedback = Feedback(name=name,email=email,location=location,ordertype=ordertype,message=message)
+
+        db.session.add(feedback)
+        db.session.commit()
+        return render_template('/')
+    else:
+        return render_template('feedback.html')
+
